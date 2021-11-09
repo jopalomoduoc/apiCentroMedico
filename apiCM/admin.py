543c1d85da50b7ad2d179af330faeca8cc176007
Jose Palomo <jo.palomo@duocuc.cl>
2021-11-09 16:11:25 -0300

    se instancian modelos en admin

--- apiCM/admin.py
+++ apiCM/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Paciente
+from .models import Paciente, Region, Comuna, Sucursal, Especialidad, Pago, Persona, Medico, Agenda
 
 # Register your models here.
 admin.site.register(Paciente)
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(Sucursal)
+admin.site.register(Especialidad)
+admin.site.register(Pago)
+admin.site.register(Persona)
+admin.site.register(Medico)
+admin.site.register(Agenda)
