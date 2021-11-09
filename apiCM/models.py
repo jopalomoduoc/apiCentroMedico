3b6ce4412b63174f03ab7591f0c6175fe2a79ec0
Jose Palomo <jo.palomo@duocuc.cl>
2021-11-09 10:25:45 -0300

    se crean modelos generales y serializers

--- apiCM/models.py
+++ apiCM/models.py
@@ -1,17 +1,71 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    nombre = models.CharField(max_length=90)
+    def __str__(self):
+      return self.nombre
+
+class Comuna(models.Model):
+    nombre =  models.CharField(max_length=90) 
+    id_region =  models.ForeignKey(Region, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.nombre
+
+class Persona(models.Model):
+    rut = models.CharField(max_length=14)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    id_comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.nombre
+
 class Paciente(models.Model):
     correo = models.CharField(primary_key=True, max_length=50, verbose_name='Correo de Usuario')
-    nombre = models.CharField(max_length=25, verbose_name='Nombre de Usuario')
-    apellido = models.CharField(max_length=25, verbose_name='Apellido de Usuario')
+    id_persona = models.ForeignKey(Persona, on_delete=models.PROTECT)
     password = models.CharField(max_length=12, verbose_name='Contraseña')
     repeatPassword = models.CharField(max_length=12, verbose_name='Repetir Contraseña')
-    direccion = models.TextField(max_length=255, verbose_name='Dirección')
     celular = models.IntegerField(verbose_name="Celular")
     enfermedades = models.TextField(max_length=255, verbose_name='Enfermedades')
     alergias = models.TextField(max_length=255, verbose_name='Alergias')
     sexo = models.CharField(max_length=15, verbose_name='Sexo')
+
+    def __str__(self):
+        return self.correo
+
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=50)
+    id_comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT) 
+    def __str__(self):
+        return self.nombre
+    
+class Especialidad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre 
+
+class Pago(models.Model):
+    tipo = models.CharField(max_length=50)
+    id_agenda = models.IntegerField(max_length=11)
+    fecha_pago = models.DateField()  
+    monto_pago = models.IntegerField(max_length=11)   
+def __str__(self):
+    return self.tipo
+
+class Medico(models.Model):
+      id_persona = models.ForeignKey(Persona, on_delete=models.PROTECT) 
+      id_especialidad = models.ForeignKey(Especialidad, on_delete=models.PROTECT)
+def __str__(self):
+    return self.id_persona         
+
+class Agenda(models.Model):
+    correo = models.ForeignKey(Paciente, on_delete=models.PROTECT)
+    id_medico =  models.ForeignKey(Medico, on_delete=models.PROTECT)  
+    fecha_inicio = models.DateField()
+    fecha_termino = models.DateField()
+    id_sucursal = models.ForeignKey(Sucursal, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.correo
--- apiCM/serializers.py
+++ apiCM/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Paciente
+from .models import Paciente, Region, Comuna, Sucursal, Especialidad, Pago, Persona, Medico, Agenda
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +10,44 @@ class UserSerializer(serializers.ModelSerializer):
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields = ['correo', 'nombre', 'apellido', 'password', 'repeatPassword', 'direccion', 'celular', 'enfermedades', 'alergias', 'sexo']
+        fields = ['correo', 'password', 'repeatPassword', 'celular', 'enfermedades', 'alergias', 'sexo']
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ['id_region', 'nombre_region']
+
+class ComunaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comuna
+        fields = ['id_comuna', 'nombre_comuna', 'id_region']
+
+class SucursalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sucursal
+        fields = ['id_sucursal', 'nombre_sucursal', 'id_comuna']
+
+class EspecialidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Especialidad
+        fields = ['id_especialidad', 'nombre_especialidad']
+
+class PagoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pago
+        fields = ['id_pago', 'tipo_pago', 'id_agenda', 'fecha_pago', 'monto_pago']
+
+class PersonaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Persona
+        fields = ['id_persona', 'rut', 'nombre', 'apellido', 'dirección', 'id_comuna']
+
+class MedicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medico
+        fields = ['id_medico', 'id_persona', 'id_especialidad']
+
+class AgendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agenda
+        fields = ['id_agenda', 'correo', 'id_medico', 'fecha_inicio', 'fecha_termino', 'id_sucursal']
