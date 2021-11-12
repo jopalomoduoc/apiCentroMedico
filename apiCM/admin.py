ffa59437ce1e61cd26995c6625ce73d9c5b87e77
Jose Palomo <jo.palomo@duocuc.cl>
2021-11-12 20:24:29 -0300

    se resetea base de datos y corrige archivo admin.py

--- apiCM/admin.py
+++ apiCM/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Paciente, Region, Comuna, Sucursal, Especialidad, Pago, Persona, Medico, Agenda
+from .models import Paciente, Region, Comuna, Sucursal, Especialidad, Pago, Agenda, Trabajador, Cargo
 
 # Register your models here.
 admin.site.register(Paciente)
@@ -8,6 +8,6 @@ admin.site.register(Comuna)
 admin.site.register(Sucursal)
 admin.site.register(Especialidad)
 admin.site.register(Pago)
-admin.site.register(Persona)
-admin.site.register(Medico)
+admin.site.register(Trabajador)
+admin.site.register(Cargo)
 admin.site.register(Agenda)
